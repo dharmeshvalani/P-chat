2935d5f5fcb9f5e337b04b0447c9b7ac9c48ac19
dharmeshvalani <dharmeshvalani61@gmail.com>
2026-03-26 15:58:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,20 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-// store room users count
-const roomUsers = {};
-
 io.on("connection", (socket) => {
 
-  socket.on("joinRoom", (roomCode) => {
-    socket.join(roomCode);
-
-    if (!roomUsers[roomCode]) {
-      roomUsers[roomCode] = 0;
-    }
-
-    if (roomUsers[roomCode] >= 2) {
-      socket.emit("roomFull");
-      return;
-    }
-
-    roomUsers[roomCode]++;
-    socket.roomCode = roomCode;
-
-    io.to(roomCode).emit("message", "User joined");
+  socket.on("joinRoom", ({ room, name }) => {
+    socket.join(room);
+    socket.to(room).emit("online");
   });
 
-  socket.on("sendMessage", ({ roomCode, message }) => {
-    io.to(roomCode).emit("message", message);
+  socket.on("sendMessage", (data) => {
+    io.to(data.room).emit("message", data);
   });
 
-  socket.on("disconnect", () => {
-    const roomCode = socket.roomCode;
-    if (roomCode && roomUsers[roomCode]) {
-      roomUsers[roomCode]--;
-      if (roomUsers[roomCode] <= 0) {
-        delete roomUsers[roomCode];
-      }
-    }
+  socket.on("typing", (data) => {
+    socket.to(data.room).emit("typing", data);
   });
+
+  socket.on("seen", ({ room }) => {
+    socket.to(room).emit("seen");
+  });
+
 });
-
-server.listen(3000, () => console.log("Server running on 3000"));
