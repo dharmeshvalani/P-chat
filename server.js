cc62055ea93c56c5c3c4b308eaaac4a2bad4d440
dharmeshvalani <dharmeshvalani61@gmail.com>
2026-03-26 18:42:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// create server
+const server = http.createServer(app);
+
+// create socket server ✅
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// socket logic
 io.on("connection", (socket) => {
 
   socket.on("joinRoom", ({ room, name }) => {
+    const clients = io.sockets.adapter.rooms.get(room);
+
+    if (clients && clients.size >= 2) {
+      socket.emit("roomFull");
+      return;
+    }
+
     socket.join(room);
     socket.to(room).emit("online");
   });
@@ -17,4 +43,15 @@ io.on("connection", (socket) => {
     socket.to(room).emit("seen");
   });
 
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+});
+
+// IMPORTANT for Render
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
